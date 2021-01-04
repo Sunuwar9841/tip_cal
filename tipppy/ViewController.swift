4d7e7a2fddc3d61867e0fe45ca1ffc7deafae22f
alisha.sunuwar <alisha.sunuwar@2603-7000-b140-3630-04f4-db2d-72c5-e8c1.res6.spectrum.com>
2021-01-04 16:12:49 -0500

    Initial Commit

---
+++ tipppy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipppy
+//
+//  Created by alisha.sunuwar on 1/4/21.
+//  Copyright © 2021 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
